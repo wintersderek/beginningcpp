34f023843d3dbe3f84c4c0067477491b03b34c23
Derek Winters <dw@Dereks-MacBook-Pro.local>
2020-03-17 16:34:36 -0400

    Updated chapter 5.

--- chap5/arrayTest.cpp
+++ chap5/arrayTest.cpp
@@ -7,13 +7,14 @@ int main () {
     const size_t array_size {200};
     unsigned int int_array [array_size] {}; // This initializes all values to 0
 
-    for (size_t i {}; i < array_size; ++i) {
+    std::cout << "Size of array_size: " << std::size(int_array) << "\n";
+    
+    for (size_t i {}; i < std::size(int_array); ++i) {
         int_array[i] = i;
     }
 
-    for (size_t i {0}; i < array_size + 2000; ++i) {
-        std::cout << "int_arra[" << i << "] = " << int_array[i] << "\n";
-    }
+    std::cout << "(sizeof(int_array)/sizeof(int_array[0])) = "
+              << (sizeof(int_array)/sizeof(int_array[0])) << "\n";
 
     return 0;
 }
